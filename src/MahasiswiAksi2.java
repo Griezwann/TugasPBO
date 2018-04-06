caf3dace7a2c660b133cbcaae574d9067d038f0e
griezwann <Cuanzplakk@gmail.com>
2018-04-06 22:42:15 +0800

    Mengisi Nilai atribut

--- src/MahasiswiAksi2.java
+++ src/MahasiswiAksi2.java
@@ -11,5 +11,11 @@
 public class MahasiswiAksi2 {
     public static void main (String[] args) {
         Mahasiswi Dini = new Mahasiswi();
+        
+        Dini.nama="Dini Yulia";
+        Dini.NPM="16630942";
+        Dini.Kelas="4B Nonreguler bjb";
+        Dini.agama="Islam";
+        Dini.IPK=3.5;
     }
 }
