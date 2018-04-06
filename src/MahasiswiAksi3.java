b3ecff6c7fd6c4eb031476370f6df824124950fd
griezwann <Cuanzplakk@gmail.com>
2018-04-06 22:56:22 +0800

    Memanggil Nilai atribut

--- src/MahasiswiAksi3.java
+++ src/MahasiswiAksi3.java
@@ -11,5 +11,11 @@
 public class MahasiswiAksi3 {
     public static void main (String[] args) {
         Mahasiswi Fina = new Mahasiswi();
+        
+        Fina.nama="Fina Lestari";
+        Fina.NPM="16630944";
+        Fina.Kelas="4B Nonreguler bjb";
+        Fina.agama="Islam";
+        Fina.IPK=3.5;
     }
 }
